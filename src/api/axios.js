edc7556f7c2c94095a48c5d21ddbde58f5da5ce7
shashismudalagi@gmail.com <shashismudalagi@gmail.com>
2026-01-21 17:42:38 +0530

    Fix API baseURL for production

--- src/api/axios.js
+++ src/api/axios.js
@@ -12,7 +12,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://backend-cvfl.onrender.com/api",
   withCredentials: true,
 });
 
