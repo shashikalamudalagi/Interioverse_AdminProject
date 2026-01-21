c2eaaeffb59dcb090c685f358d15d40bea90cfa2
shashismudalagi@gmail.com <shashismudalagi@gmail.com>
2026-01-21 13:46:33 +0530

    Fix API baseURL for Render backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,21 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: process.env.REACT_APP_API_URL || "https://interioverse-backend-bpf6.onrender.com/",
+//   withCredentials: true,    // cookies (JWT)
+// });
+
+// export default api;
+
+
+// //http://localhost:5000/api
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://interioverse-backend-bpf6.onrender.com/",
-  withCredentials: true,    // cookies (JWT)
+  baseURL:
+    process.env.REACT_APP_API_URL ||
+    "https://interioverse-backend-bpf6.onrender.com/api",
+  withCredentials: true,
 });
 
 export default api;
-
-
-//http://localhost:5000/api
